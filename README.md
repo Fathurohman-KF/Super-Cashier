b4f9c9763a47fefe9b4eae3840abb1e687980e7a
Fathurohman-KF <f.k.fauzan@gmail.com>
2024-02-23 10:33:19 +0700

    first commit super cashier

---
+++ README.md
@@ -0,0 +1,69 @@
+#Latar Belakang
+Andi adalah seorang pemilik supermarket besar di salah satu kota di Indonesia. Andi memiliki rencana untuk melakukan perbaikan proses bisnis, yaitu Andi akan membuat sistem kasir yang self-service di supermarket miliknya. Sehingga customer bisa langsung memasukkan item yang dibell, jumlah item yang dibell, dan harga item yang dibell dan fitur yang lain.
+Sehingga customer yang tidak berada di kota tersebut bisa membeli barang dari supermarket tersebut. Setelah Andi melakukan riset, ternyata Andi memiliki masalah, yaitu Andi membutukan Programmer untuk membuatkan fitur - fitur agar bisa sistem kasir self-service di supermarket itu bisa berjalan dengan lancar
+
+#Requirements atau Objective
+
+1. Costumer membuat ID transaksi customer dengan membuat object dari class. Sebagai contoh, trnsct_123 = Transaction()
+2. Customer meng-input nama item, jumlah item, dan harga barang dengan method add_item()
+3. Jika ternyata ada kesalahan dalam meng-input nama item atau jumlah item atau harga item tetapi tidak ingin menghapus itemnya, customer bisa melakukan:
+
+- Update nama item dengan method update_item_name()
+- Update jumlah item dengan method update_item_qty()
+- Update harga item dengan method update_item_price()
+
+4. Jika batal membeli item belanjaan, Customer bisa melakukan:
+
+- Menghapus salah satu item dari nama item dengan method delete_item()
+- Langsung menghapus semua transaksi atau reset transaksi dengan method reset_transaction()
+
+5. Customer bisa melakukan pengecekan pada status pemesanan sebelum membayar dengan method check_order() yang akan mengeluarkan output yaitu:
+
+- Akan mengeluarkan pesan "Order is correct" jika tidak ada kesalahan input.
+- Akan mengeluarkan pesan "There is a data input error" jika terjadi kesalahan input.
+
+6. Keluaran output transaksi atau pemesanan apa saja yang sudah dibeli.
+   Setelah melakukan checkout, Customer bisa menghitung total belanja dengan method total_price. Pada supermarket terdapat ketentuan:
+
+- Jika total belanja diatas Rp 200.000 maka akan mendapatkan diskon 5%
+- Jika total belanja diatas Rp 300.000 maka akan mendapatkan diskon 8%
+- Jika total belanja diatas Rp 500.000 maka akan mendapatkan diskon 10%
+
+#Flowchart
+
+#Penjelasan
+
+1. data_item = { "Nama item" : [], "Jumlah item" : [], "Harga item" : [], "Total harga" : []}
+   = Untuk menampung data dalam transaksi
+2. add_item (name, qty, price) = Untuk mnambahkan item di list transaksi
+3. update_item_name (name, update_name) = untuk mengupdate nama item di dalam transaksi
+4. update_item_qty (qty, update_qty) = Untuk mengupdate jumlah item pada transaksi
+5. update_item_price (price, update_price) = Untuk melakukan perubahan harga pada transaksi
+6. delete_item (delete_update) = Untuk menghapus atau membatalkan pada transaksi
+7. reset_transaction() = Untuk membatalkan semua transaksi
+8. check_order() = Untuk melakukan pengecekan pada seluruh item transaksi
+9. total_price() = Untuk menghitung total harga belanja pada seluruh item transaksi
+
+#Test Case
+##Test 1:
+###Customer ingin menambahkan dua item baru menggunakan method add_item () . Item yang ditambahkan adalah sebagai berikut:
+• Nama Item: Ayam Goreng, Qty: 2, Harga: 20000
+• Nama Item: Pasta Gigi, Qty: 3, Harga: 15000
+
+##Test 2
+###Ternyata Customer salah membeli salah satu item dari belanjaan yang sudah ditambahki maka Customer menggunakan method delete_item () untuk menghapus item. Item y ingin dihapuskan adalah Pasta Gigi.
+
+##Test 3
+###Ternyata setelah dipikir - pikir Customer salah memasukkan item yang ingin dibelanjakan!
+###Daripada menghapusnya satu - satu, maka Customer cukup menggunakan method reset_transaction () untuk menghapus semua item yang sudah ditambahkan.
+
+##Test 4
+###Setelah Customer selesai berbelanja, akan menghitung total belanja yang harus dibay menggunakan method total_price () . Sebelum mengeluarkan output total belanja menampilkan item - item yang dibeli.
+
+#Kesimpulan
+Andi membuat program supermarket yang bertujuan untuk memudahkan customer dalam melakukan transaski di dalam supermarket.
+Supermarket telah menjadi tempat yang sangat membantu bagi kebanyakan orang untuk memperoleh barang atau makanan yang mereka butuhkan. Supermarket menyediakan berbagai barang dalam satu tempat, memungkinkan orang untuk berbelanja secara efisien. Program ini akan meningkatkan efisiensi baik bagi customer maupun supermarket.
+
+#Saran Pengembangan
+*Bisa menambahkan database agar memperlancar program
+*Pengembangan dalam metode pembayaran
---
+++ Script.py
@@ -0,0 +1,146 @@
+from tabulate import tabulate
+
+class Transaction:
+    data_item = { "Nama item" :  [], "Jumlah item" : [], "Harga item" : [], "Total harga" : []}
+
+    def add_item(self, name, qty, price):
+        try:
+            # Cek tipe data
+            if type(name) != str:
+                raise TypeError("Nama item harus berupa string.")
+            if type(qty) != int:
+                raise TypeError("Jumlah item harus berupa integer.")
+            if type(price) != int:
+                raise TypeError("Harga item harus berupa integer.")
+
+            # Menambahkan item ke transaksi
+            self.data_item["Nama item"].append(name)
+            self.data_item["Jumlah item"].append(qty)
+            self.data_item["Harga item"].append(price)
+            total = qty * price
+            self.data_item["Total harga"].append(total)
+
+            return print(f'Harga item yang dibeli {self.data_item}')
+        except TypeError:
+            print(f"Terjadi kesalahan: Tipe data salah")
+
+    def update_item_name(self, name, update_name ):
+        try:
+            if type(name) != str:
+                raise TypeError("Nama item harus berupa string.")
+            if type(update_name) != str:
+                raise TypeError("Nama item harus berupa string.")
+            
+            name_update = self.data_item["Nama item"].index(name)
+            self.data_item["Nama item"][name_update] = update_name
+            return print(f'Data nama item diubah{self.data_item}')
+        
+        except TypeError:
+            print(f'Terjadi Kesalahan: Tipe data harus string')
+        except ValueError:
+            print(f'Nama item {update_name} tidak ditemukan dalam data.')
+
+
+    def update_item_qty(self, qty, update_qty):
+        try:
+            if type(qty) != str:
+                raise TypeError("Nama item harus berupa string.")
+            if type(update_qty) != str:
+                raise TypeError("Nama item harus berupa string.")
+
+            qty_update = self.data_item["Jumlah item"].index(qty)
+            self.data_item["Jumlah item"][qty_update] = update_qty
+            self.data_item["Total harga"][qty_update] = update_qty * self.data_item["Harga item"][qty_update]
+            return print(f'Data item yang diubah{self.data_item}')
+           
+        except TypeError:
+           print(f'Terjadi kesalahan: Tipe data harus Integer')
+        except ValueError:
+            print(f'Nama item {update_qty} tidak ditemukan dalam data.')
+    
+    def update_item_price(self, price, update_price):
+        try:
+            if type(price) != str:
+                raise TypeError("Nama item harus berupa string.")
+            if type(update_price) != str:
+                raise TypeError("Nama item harus berupa string.")
+
+            # Update harga dari suatu item
+            price_update = self.data_item["Harga item"].index(price)
+            self.data_item["Harga item"][price_update] = update_price
+            self.data_item["Total harga"][price_update] = update_price * self.data_item["Jumlah item"][price_update]
+            return print(f'Harga yang diupdate dari item {self.data_item}')
+        
+        except TypeError:
+            print(f'Terjadi kesalahan: Tipe data harus Integer')
+        except ValueError:
+            print(f'Nama item {update_price} tidak ditemukan dalam data.')
+
+    def delete_item(self, delete_update):
+        try:
+            if type(delete_update) != str:
+                raise TypeError("Nama item yang akan dihapus harus berupa string.")
+            
+            delete_index = self.data_item['Nama item'].index(delete_update)
+            for key in self.data_item:
+                self.data_item[key].pop(delete_index)
+            return print(f'Data item yang dihapus{self.data_item}')
+            
+        except TypeError:
+            print(f'Terjadi Kesalahan: Tipe data harus string')
+        except ValueError:
+            print(f'Nama item {delete_update} tidak ditemukan dalam data.')
+
+    def reset_transaction(self):
+        '''
+        Menghapus semua item
+        '''
+        for key in self.data_item:
+            self.data_item[key].clear()
+        return print(f'Semua item dihapus')
+    
+    def check_order(self):
+        '''
+        Melakukan pengecekan terhadap seluruh item apakah terdapat nilai null
+        '''
+        try:
+
+        # Membuat dan mencetak tabel
+            tabel = list(zip(*self.data_item.values()))
+            headers = list(self.data_item.keys())
+            tabel_str = tabulate(tabel, headers, tablefmt='github')
+            print(tabel_str)
+
+            check = True
+            for item in self.data_item['Total harga']:
+                if item <= 0:
+                    check = False
+            if check:
+                print("Order tersedia")
+            else:
+                print("Order tidak ada")
+        except TypeError:
+            print(f"Terjadi kesalahan: Pastikan semua item memiliki harga.")
+
+    def total_price(self):
+        '''
+        Menghitung total belanja dari transaksi, jika pembelian
+        - lebih dari 200_000 maka mendapat diskon 5%
+        - lebih dari 300_000 maka mendapat diskon 8%
+        - lebih dari 500_000 maka mendapat diskon 10%
+        '''
+        try:
+            total = sum(self.data_item['Total harga'])
+
+            if total < 200000:
+                pass
+            elif total < 300000:
+                total = total * 0.95
+            elif total < 500000:
+                total = total * 0.92
+            else:
+                total = total * 0.9
+
+            return print(f'Total price : {total}')
+        except TypeError :
+            print(f"Terjadi kesalahan: Pastikan semua item memiliki harga.")
